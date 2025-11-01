7d3992a322fb0d6733b56a0a3352cb9fd9d66e10
Douglas Dias <dougladias@hotmail.com>
2025-11-01 13:37:08 -0400

    atualizaçaõ de cors

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -9,7 +9,12 @@ import { swaggerSpec } from './config/swagger';
 const app: Application = express();
 
 // Middlewares globais
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
